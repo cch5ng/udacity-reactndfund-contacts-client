d2853e6dbb8d6203bfc05f7dab6b0b91bdb0c2a4
Carol Chung <cch5ng@gmail.com>
2017-08-12 15:45:29 -0700

    refactor ListContacts to reg comp to use state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ListContacts = (props) => {
+class ListContacts extends React.Component {
+  state = {
+    query: ""
+  }
+
+  render() {
+    const { contacts, deleteContact} = this.props;
     return (
       <ol className="ordered-list">
-        {props.contacts.map((contact, idx) => (
+        {contacts.map((contact, idx) => (
           <li key={contact.id} className="contact-list-item">
             <div className="contact-avatar" style={{
               backgroundImage: `url(${contact.avatarURL})` 
@@ -13,13 +19,17 @@ const ListContacts = (props) => {
               <p>{contact.name}</p>
               <p>{contact.email}</p>
             </div>
-            <button id={contact.id} className="contact-remove" onClick={(ev) => props.deleteContact(ev.target.id)}>
+            <button id={contact.id} className="contact-remove" onClick={(ev) => deleteContact(ev.target.id)}>
             </button>
           </li>
         ))}
 
       </ol>
     )
+
+  }
+
+
 }
 
 ListContacts.proptypes = {
