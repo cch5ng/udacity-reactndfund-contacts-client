b17db58eda0c99adba246032f085efb08eee2e80
Carol Chung <cch5ng@gmail.com>
2017-08-12 14:59:45 -0700

    change ListContacts to stateless funct comp; add prop types obj

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class ListContacts extends Component {
-
-  render() {
-    const { contacts, deleteContact } = this.props;
+const ListContacts = (props) => {
     return (
       <ol className="ordered-list">
-        {contacts.map((contact, idx) => (
+        {props.contacts.map((contact, idx) => (
           <li key={contact.id} className="contact-list-item">
             <div className="contact-avatar" style={{
               backgroundImage: `url(${contact.avatarURL})` 
@@ -15,16 +13,18 @@ class ListContacts extends Component {
               <p>{contact.name}</p>
               <p>{contact.email}</p>
             </div>
-            <button id={contact.id} className="contact-remove" onClick={(ev) => deleteContact(ev.target.id)}>
+            <button id={contact.id} className="contact-remove" onClick={(ev) => props.deleteContact(ev.target.id)}>
             </button>
           </li>
         ))}
 
       </ol>
     )
+}
 
-  }
-
+ListContacts.proptypes = {
+  contacts: PropTypes.array.isRequired,
+  deleteContact: PropTypes.func.isRequired
 }
 
 export default ListContacts
